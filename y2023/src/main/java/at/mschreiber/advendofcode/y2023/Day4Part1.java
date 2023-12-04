2ac0b5a1d832eed10a0c67f2f69efa6d5d8dd66b
Martin Schreiber <martin@mschreiber.at>
2023-12-04 07:17:07 +0100

    Update Day4Part1.java

--- y2023/src/main/java/at/mschreiber/advendofcode/y2023/Day4Part1.java
+++ y2023/src/main/java/at/mschreiber/advendofcode/y2023/Day4Part1.java
@@ -2,28 +2,30 @@ package at.mschreiber.advendofcode.y2023;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Day4Part1 {
 
   public static void main(String[] args) {
-    Pattern p = Pattern.compile("\\d+");
     String[] cards = Day4Input.mine.split(("\r\n"));
     double sum = 0;
     for (String card : cards) {
-      long cnt = 0;
       List<Long> cNbs = Arrays.asList(card.split("[|:]")[1].replaceAll("( )+", " ").trim().split(" ")).stream()
           .map(Long::parseLong).distinct().collect(Collectors.toList());
-      String s = card.split("[|:]")[2].trim();
       List<Long> mNbs = Arrays.asList(card.split("[|:]")[2].replaceAll("( )+", " ").trim().split(" ")).stream()
-          .map(Long::parseLong).distinct().collect(Collectors.toList());
-      for (Long x : mNbs) {
-        if (cNbs.contains(x)) {
-            cnt++;
+          .map(Long::parseLong).collect(Collectors.toList());
+      long cnt = 0;
+      for (Long x : cNbs) {
+        if (mNbs.contains(x)) {
+          cnt++;
         }
       }
+      if (cnt > 1) {
       sum += Math.pow(2, cnt - 1);
+      }
+      if (cnt == 1) {
+        sum++;
+      }
 
     }
     System.err.println(sum);
